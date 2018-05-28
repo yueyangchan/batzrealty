1b2d20aa1b5d2d5340b4734785c97fb307ca1903
ars3697 <ars3697@uw.edu>
2018-05-28 00:26:41 -0700

    Added basic ui/server setup

--- ui.R
+++ ui.R
@@ -1,36 +1,30 @@
+
+# install.packages("shiny")
+
 library("shiny")
-shinyUI(
-  fluidPage(
-    titlePanel("Using Zillow API"),
-    sidebarLayout(
-      # create widgets
-      sidebarPanel(
-    
-      ),
-      mainPanel(
-        # create tabset panel to display table and plot neatly
-        tabsetPanel(
-          type = "tabs",
-          tabPanel(
-            p(
-            "The correlation between distance from the neighborhood
-            to downtown Seattle and the the average price of the 
-            neighborhood is ", textOutput("correlation", inline = TRUE),
-            ". Although there isn't strong correlation between distance
-            between the center of seattle and the price of the location,
-            distance the center of seattle may play a role in the average
-            price of the house along with many other factors."),
-            "Plot",
-            # create interactive click plot
-            plotOutput("plot", click = "plot_click"),
-            # display city that is currently selected
-            p(
-              "Selected City:",
-              strong(textOutput("selected_city", inline = TRUE))
-            )
-          )
-          )
-        )
-      )
-    )
+
+ui <- navbarPage(
+  title = "Zillow",
+  
+  tabPanel(
+    title = "1",
+    titlePanel("1")
+  ),
+  
+  tabPanel(
+    title = "2",
+    titlePanel("2")
+  ),
+
+  tabPanel(
+    title = "3",
+    titlePanel("3")
+  ),
+  
+  tabPanel(
+    title = "4",
+    titlePanel("ars3697")
+  )
 )
+
+shinyUI(ui)
