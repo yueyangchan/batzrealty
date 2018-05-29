6093002f6c3b5b47c6e375c93e5996d5071c27b0
ars3697 <ars3697@uw.edu>
2018-05-29 13:13:54 -0700

    Update ui.R, server.R, add data files

--- server.R
+++ server.R
@@ -1,50 +1,80 @@
 
 # install.packages("shiny")
-# install.packages("xml2")
+# install.packages("XML")
 # install.packages("jsonlite")
 # install.packages("dplyr")
 
 library("shiny")
-library("xml2")
+library("XML")
 library("jsonlite")
 library("dplyr")
 
-base_uri <- "http://www.zillow.com/webservice/GetRegionChildren.htm"
-z_id_ars3697 <- "X1-ZWz18lzviceeiz_9skq6"
+# File as of 5-29-18.
+all_state <- read.csv("data/State_Zhvi_AllHomes.csv", stringsAsFactors = FALSE)
+all_city <- read.csv("data/City_Zhvi_AllHomes.csv", stringsAsFactors = FALSE)
 
-# state_df (function) - Returns a dataframe of all of a state's cities/counties and their "zindex" in USD.
-# state (string) - State in the United States. Should be writen in two letters (ex. "wa" for Washington, "ca" for California).
-state_df <- function(state) {
-  uri <- paste0(base_uri, "?zws-id=", z_id_ars3697, "&state=", state)
-  # Zillow returns the response back in XML.
-  response <- xmlParse(uri)	
-  # Cleaning up XML and turning it into a cleaner looking dataframe.
-  list <- response %>% xmlToList() %>% toJSON(pretty = TRUE) %>% fromJSON()	
-  # Getting rid of the first index, because it's just a "count" row. It's irrelevant.
-  raw_data <- list$response$list[-1]
-  # Creating a new dataframe with two columns: "name" and "zindex_usd".
-  # Only has one row, the first row from the raw_data dataframe.
-  state_data <- data_frame(name = raw_data[1]$region$name,
-                           zindex_usd = raw_data[1]$region$zindex$text)
-  
-  # Adds in the rest of the rows into the state_data dataframe.
-  # tryCatch is used to prevent error messages in the console.
-  for(i in 2:NROW(raw_data)) {
-    tryCatch({
-      temp_df <- data.frame(name = raw_data[i]$region$name,
-                          zindex_usd = raw_data[i]$region$zindex$text)
-      state_data <- rbind(state_data, temp_df)}, error = function(e){})
-  }
-  
-  state_data
-}
+# QUESTION
+# What state/city is growing the fastest? In terms of increasing housing prices.
+# Increasing housing prices can be interpreted as a fast growing state/city. 
+# This information can help new/young professionals identify which city they can potentially move to, or avoid, when they first start their career.
 
-View(state_df("ca"))
+# PSEUDOCODE
+# Pick from a date range: let user choose year and month, then make that into a string for easy access.
+# Pick from past to present dates (ex. X1994.04, X2009.06)
+# Calculate the percent increase from past date to present date for each state (make sure to take into account missing dates: North Dakota)
+# Display visualization of a map of the UNITED STATES.
+# Color each state by rate of increase.
+# Add mouse effects for addition data.
+# Maybe add a table underneath for indepth data on a single state?
+# Do another thing for all cities in a state. Just show a graph, sorted by the city with the highest rate of growth.
 
 server <- function(input, output) {
+  output$state_rate_plot
   
+  output$city_rate_table
 }
 
 shinyServer(server)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+# base_uri <- "http://www.zillow.com/webservice/GetRegionChildren.htm"
+# z_id_ars3697 <- "X1-ZWz18lzviceeiz_9skq6"
+# 
+# # state_df (function) - Returns a dataframe of all of a state's cities/counties and their "zindex" in USD.
+# # state (string) - State in the United States. Should be writen in two letters (ex. "wa" for Washington, "ca" for California).
+# state_df <- function(state) {
+#   uri <- paste0(base_uri, "?zws-id=", z_id_ars3697, "&state=", state)
+#   # Zillow returns the response back in XML.
+#   response <- xmlParse(uri)	
+#   # Cleaning up XML and turning it into a cleaner looking dataframe.
+#   list <- response %>% xmlToList() %>% toJSON(pretty = TRUE) %>% fromJSON()	
+#   # Getting rid of the first index, because it's just a "count" row. It's irrelevant.
+#   raw_data <- list$response$list[-1]
+#   # Creating a new dataframe with two columns: "name" and "zindex_usd".
+#   # Only has one row, the first row from the raw_data dataframe.
+#   state_data <- data_frame(name = raw_data[1]$region$name,
+#                            zindex_usd = raw_data[1]$region$zindex$text)
+#   
+#   # Adds in the rest of the rows into the state_data dataframe.
+#   # tryCatch is used to prevent error messages in the console.
+#   for(i in 2:NROW(raw_data)) {
+#     tryCatch({
+#       temp_df <- data.frame(name = raw_data[i]$region$name,
+#                             zindex_usd = raw_data[i]$region$zindex$text)
+#       state_data <- rbind(state_data, temp_df)}, error = function(e){})
+#   }
+#   
+#   state_data
+# }
--- ui.R
+++ ui.R
@@ -1,29 +1,59 @@
 
 # install.packages("shiny")
+# install.packages("dplyr")
 
 library("shiny")
+library("dplyr")
+
+all_city <- read.csv("data/City_Zhvi_AllHomes.csv", stringsAsFactors = FALSE)
+state_names <- all_city %>% select(State) %>% unique() %>% arrange(State)
+state_names <- state_names[,1]
+
+years <- c(1996:2018)
+months <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
 
 ui <- navbarPage(
   title = "Zillow",
-  
-  tabPanel(
-    title = "1",
-    titlePanel("1")
-  ),
-  
-  tabPanel(
-    title = "2",
-    titlePanel("2")
-  ),
 
   tabPanel(
-    title = "3",
-    titlePanel("3")
-  ),
-  
-  tabPanel(
-    title = "4",
-    titlePanel("ars3697")
+    title = "Rate of Growth",
+    sidebarPanel(
+      p("EARLIEST DATE AVAILABLE: ", strong("1996-04")),
+      p("MOST RECENT DATE AVAILABLE: ", strong("2018-04")),
+      selectInput("start_year",
+                  label = "Starting Year",
+                  choices = years),
+      selectInput("start_month",
+                  label = "Starting Month",
+                  choices = months),
+      selectInput("end_year",
+                  label = "Ending Year",
+                  choices = years),
+      selectInput("end_month",
+                  label = "Ending Month",
+                  choices = months),
+      selectInput("states",
+                  label = "States (for Cities tab)",
+                  choices = state_names),
+      actionButton("submit", 
+                   label = "Submit")
+    ),
+    
+    mainPanel(
+      navbarPage(
+        title = "Rate of Growth",
+        
+        tabPanel(
+          title = "States",
+          plotOutput("state_rate_plot")
+        ),
+      
+        tabPanel(
+          title = "Cities",
+          tableOutput("city_rate_table")
+        )
+      )
+    )
   )
 )
 
