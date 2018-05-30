a3a6241ccfa30c5307bdb3ea209bf3d477d16e43
ars3697 <ars3697@uw.edu>
2018-05-30 10:44:13 -0700

    Merged server.R and ui.R: ars3697

--- server.R
+++ server.R
@@ -10,31 +10,19 @@ library("jsonlite")
 library("dplyr")
 library("ggplot2")
 
-# File as of 5-29-18.
-all_state <- read.csv("data/State_Zhvi_AllHomes.csv", stringsAsFactors = FALSE)
-all_city <- read.csv("data/City_Zhvi_AllHomes.csv", stringsAsFactors = FALSE)
-
-state_map_data <- map_data("state")
-
 # create shiny server function with regular input and output
 # parameters along with a session for changing the input slider
 # bar based on filtered smallest and largest values
-shinyServer(function(input, output, session) {
+server <- function(input, output) {
   
-  # Dataframe to be used to plot a map of the United States.
-  state_data <- reactive({
-    per_inc_state <- all_state %>%
-      select(start_date(), end_date()) %>%
-      summarize(per_inc = ((all_state[end_date()] - all_state[start_date()]) / all_state[start_date()] * 100))
-    
-    new_state <- all_state %>%
-      select(RegionName)
-    new_state$per_inc <- per_inc_state$per_inc
-    new_state$RegionName <- tolower(new_state$RegionName)
-    
-    combined_state <- left_join(state_map_data, new_state, by = c("region" = "RegionName"))
-    combined_state  
-  })
+  ####################
+  # Zechariah Cheung #
+  ####################
+  
+  base_uri <- "http://www.zillow.com/webservice/GetRegionChildren.htm"
+  z_id <- "X1-ZWz1geilg38iyz_9r65p"
+  state <- "wa"
+  childtype <- "neighborhood"
   
   # given a city, it will return all the neighborhoods
   # in that city that have a complete set of data. Many
@@ -106,6 +94,87 @@ shinyServer(function(input, output, session) {
     add_city_df("lynnwood")
   )
 
+  # calculate the distance from the center 
+  # of seattle for each row in the data table
+  for(i in 1:NROW(realstate_df)){
+    realstate_df$dist[i] <- earth.dist(
+      as.double(realstate_df$long[i]),
+      as.double(realstate_df$lat[i]),
+      seattle_lat_long[2],
+      seattle_lat_long[1]
+    )
+  }
+  realstate_df$price <- as.numeric(realstate_df$price)
+  
+  realstate_reactive <- reactive({
+    return(realstate_df)
+  })
+  
+  # create variable that stores the value that is being clicked on
+  clicked_city <- reactiveValues()
+  clicked_city$city <- ""
+  
+  # observe when plot is clicked on to set city 
+  # that was clicked on to a new color
+  observeEvent(input$plot_click, {
+    selected <- nearPoints(realstate_reactive(), input$plot_click)
+    clicked_city$city <- unique(selected$city)
+  })
+  
+  # output the city that was clicked on
+  output$selected_city <- renderText({
+    return(clicked_city$city)
+  })
+  
+  # output the intext correlation text
+  output$correlation <- renderText({
+    cor(realstate_df$dist, realstate_df$price)
+  })
+  
+  # create interactive/reactive plot of eviction filings versus race
+  output$plot <- renderPlot({
+    p <- ggplot(
+      data = realstate_reactive(),
+      mapping = aes(x = dist, y = price)
+    ) +
+      geom_smooth(mapping = aes(x = dist, y = price, alpha = 1)) +
+      # create point plot with points in the same city
+      # as the clicked point colored a contrasting color
+      geom_point(aes(color = (city %in% clicked_city$city)), size = 3) +
+      guides(color = FALSE) +
+      labs(
+        title = "Price in USD vs Distance from Downtown in miles",
+        x = "Distance",
+        y = "Price"
+      ) 
+    return(p)
+  })
+  
+  #####################
+  # Addison Sengvilay #
+  #####################
+  
+  # File as of 5-29-18.
+  all_state <- read.csv("data/State_Zhvi_AllHomes.csv", stringsAsFactors = FALSE)
+  all_city <- read.csv("data/City_Zhvi_AllHomes.csv", stringsAsFactors = FALSE)
+  
+  state_map_data <- map_data("state")
+  
+  # Dataframe to be used to plot a map of the United States.
+  state_data <- reactive({
+    per_inc_state <- all_state %>%
+      select(start_date(), end_date()) %>%
+      summarize(per_inc = ((all_state[end_date()] - all_state[start_date()]) / all_state[start_date()] * 100))
+    
+    new_state <- all_state %>%
+      select(RegionName)
+    new_state$per_inc <- per_inc_state$per_inc
+    new_state$RegionName <- tolower(new_state$RegionName)
+    
+    combined_state <- left_join(state_map_data, new_state, by = c("region" = "RegionName"))
+    combined_state  
+  })
+  
   # Dataframe for state data.
   state_data_table <- reactive({
     per_inc_state <- all_state %>%
@@ -188,4 +257,6 @@ shinyServer(function(input, output, session) {
   output$city_rate_table <- renderDataTable({
     city_data()
   })
-})
+}
+
+shinyServer(server)
--- ui.R
+++ ui.R
@@ -13,8 +13,44 @@ years <- c(1996:2018)
 months <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
 
 ui <- navbarPage(
-  title = "Zillow",
-
+  title = "Using Zillow API",
+  
+  # Zechariah Cheung
+  tabPanel(
+    title = "Distance from Seattle",
+    sidebarLayout(
+      # create widgets
+      sidebarPanel(
+        
+      ),
+      mainPanel(
+        # create tabset panel to display table and plot neatly
+        tabsetPanel(
+          type = "tabs",
+          tabPanel(
+            p(
+              "The correlation between distance from the neighborhood
+            to downtown Seattle and the the average price of the 
+            neighborhood is ", textOutput("correlation", inline = TRUE),
+              ". Although there isn't strong correlation between distance
+            between the center of seattle and the price of the location,
+            distance the center of seattle may play a role in the average
+            price of the house along with many other factors."),
+            "Plot",
+            # create interactive click plot
+            plotOutput("plot", click = "plot_click"),
+            # display city that is currently selected
+            p(
+              "Selected City:",
+              strong(textOutput("selected_city", inline = TRUE))
+            )
+          )
+        )
+      )
+    )
+  ),
+  
+  # Addison Sengvilay
   tabPanel(
     title = "Rate of Growth",
     sidebarPanel(
@@ -52,41 +88,41 @@ ui <- navbarPage(
           title = "States",
           titlePanel("Zillow Home Value Index Percentage Change by State"),
           p("Zillow offers a zIndex value, which is the Zillow Home Value Index (ZHVI). 
-            It is a measure of the median estimated home value across a given region and 
-            housing type. The plot and table below details the percentage change of the 
-            zIndex value across an entire state between a given timeframe. This data can 
-            help answer the question of which state/city is growing the fastest/slowest 
-            throughout time. This data can be useful, because increasing housing prices can be 
-            interpreted as a fast growing state/city. This information can help new/young professionals 
-            identify which city they can potentially move to, or avoid, when they first start their career."),
+          It is a measure of the median estimated home value across a given region and 
+          housing type. The plot and table below details the percentage change of the 
+          zIndex value across an entire state between a given timeframe. This data can 
+          help answer the question of which state/city is growing the fastest/slowest 
+          throughout time. This data can be useful, because increasing housing prices can be 
+          interpreted as a fast growing state/city. This information can help new/young professionals 
+          identify which city they can potentially move to, or avoid, when they first start their career."),
           plotOutput("state_rate_plot", width = "100%", height = "600px"),
           p("In the plot above, the darker a state's color, the less percent change they have. The 
-            lighter a state's color, the more percent change they have. This plot can be used to see 
-            the overall trend in percent change throughout a given date range."),
+          lighter a state's color, the more percent change they have. This plot can be used to see 
+          the overall trend in percent change throughout a given date range."),
           p("In the table below, there are four columns of importance. The State column gives the state 
-            in question. The second column produces the zIndex under the starting date. The third column 
-            produces the zIndex under the ending date. The Percent Change column gives the percent change 
-            between the starting and ending date. This table provides an at-a-glance overview of all states 
-            and their overall growth in price."),
+          in question. The second column produces the zIndex under the starting date. The third column 
+          produces the zIndex under the ending date. The Percent Change column gives the percent change 
+          between the starting and ending date. This table provides an at-a-glance overview of all states 
+          and their overall growth in price."),
           tableOutput("state_rate_table")
         ),
-      
+        
         tabPanel(
           title = "Cities",
           titlePanel("Zillow Home Value Index Percentage Change by City"),
           p("Zillow offers a zIndex value, which is the Zillow Home Value Index (ZHVI). 
-            It is a measure of the median estimated home value across a given region and 
-            housing type. The plot and table below details the percentage change of the 
-            zIndex value across an entire state between a given timeframe. This data can 
-            help answer the question of which state/city is growing the fastest/slowest 
-            throughout time. This data can be useful, because increasing housing prices can be 
-            interpreted as a fast growing state/city. This information can help new/young professionals 
-            identify which city they can potentially move to, or avoid, when they first start their career."),
+          It is a measure of the median estimated home value across a given region and 
+          housing type. The plot and table below details the percentage change of the 
+          zIndex value across an entire state between a given timeframe. This data can 
+          help answer the question of which state/city is growing the fastest/slowest 
+          throughout time. This data can be useful, because increasing housing prices can be 
+          interpreted as a fast growing state/city. This information can help new/young professionals 
+          identify which city they can potentially move to, or avoid, when they first start their career."),
           p("In the table below, there are six available columns of information about a specific state, and its cities.
-            The City column gives the name of a city within a given state. The State column gives the state
-            that is selected by the user. The Metro columns gives the name of the metro area in the city. The
-            County column gives the county that a given city is in. The fifth column gives information about the
-            zIndex of the starting date. The sixth columns gives information about the zIndex of the ending date."),
+          The City column gives the name of a city within a given state. The State column gives the state
+          that is selected by the user. The Metro columns gives the name of the metro area in the city. The
+          County column gives the county that a given city is in. The fifth column gives information about the
+          zIndex of the starting date. The sixth columns gives information about the zIndex of the ending date."),
           dataTableOutput("city_rate_table")
         )
       )
